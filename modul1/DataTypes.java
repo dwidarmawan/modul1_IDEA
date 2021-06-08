6a9300cfb34dfa436305b3f2d31deb9795eb47ac
dwidarmawan <81225366+dwidarmawan@users.noreply.github.com>
2021-06-08 05:41:09 -0700

    Input dengan Scanner

--- modul1/DataTypes.java
+++ modul1/DataTypes.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[] args){
@@ -11,6 +12,21 @@ public class DataTypes{
   char nilaiAbjad = 'A';
   boolean tampan = true;
 
+  Scanner inputan = new Scanner(System.in);
+
+  System.out.println("========== Fase input ==========");
+  System.out.print("Input nama depan : ");
+  namaDepan = inputan.nextLine();
+  System.out.print("Input usia : ");
+  usia = inputan.nextInt();
+  System.out.print("Input ipk : ");
+  ipk = inputan.nextDouble();
+  System.out.print("Input nilai abjad : ");
+  nilaiAbjad = inputan.next().charAt(0);
+  System.out.print("Input Tampan : ");
+  tampan = inputan.nextBoolean();
+
+  System.out.println("========== Fase output ==========");
   System.out.println("Nama depan : " + namaDepan);
   System.out.println("Nama belakang : " + namaBelakang);
   System.out.println("Usia : " + usia);
