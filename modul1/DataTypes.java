d488ab99db1ed18d1fb302f08a2d44e2f8b8018d
dwidarmawan <81225366+dwidarmawan@users.noreply.github.com>
2021-06-08 04:36:04 -0700

    First commit

---
+++ modul1/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+  String namaDepan = "Dwi";
+  String namaBelakang = "Darmawan";
+  int usia = 20;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+
+ }
+}
